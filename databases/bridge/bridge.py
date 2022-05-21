a95ec2d13c61adb1ee302f58172f1df59366854c
Raghu Ravi <raghul.roshan@gmail.com>
2022-05-21 10:53:26 +0530

    Added codebase with isolation

--- databases/bridge/bridge.py
+++ databases/bridge/bridge.py
@@ -1,7 +1,9 @@
-import ConfigParser
+from databases.runner import transcation_isolation
 
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.declarative import declarative_base
+from typing import Any
 
 BASE = declarative_base()
 
@@ -10,14 +12,18 @@ class Shipment(BASE):
 	__tablename__ = 'shipment'
     
 	id = Column(Integer, primary_key=True)
-    order_number = Column(String)
-    ship_service = Column(String)
-    requested_warehouse_code = Column(String)
-    order_status = Column(String)
+	order_number = Column(String)
+	ship_service = Column(String)
+	requested_warehouse_code = Column(String)
+	order_status = Column(String)
 
-	def __repr__(self):
-        return f"<shipment(id='{self.id}', order_number='{self.order_number}', ship_service={self.ship_service}, requested_warehouse_code={self.requested_warehouse_code}, order_status={self.order_status}>"
+class ShipmentUtils(Shipment):
+	def __init__(self, *args: Any, **kwargs: Any) -> None:
+		super().__init__(*args, **kwargs)
 
-	class ShipmentUtils:
-		pass
+	def __repr__(self) -> str:
+		return f"<shipment(id='{self.id}', order_number='{self.order_number}', ship_service={self.ship_service}, requested_warehouse_code={self.requested_warehouse_code}, order_status={self.order_status}>"
 
+	@transcation_isolation
+	def get_shipment(self, session: Session) -> str:
+		return session.query(Shipment).first()
--- databases/runner.py
+++ databases/runner.py
@@ -1,12 +1,25 @@
 from configparser import ConfigParser
 
+
+from functools import wraps
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import Session
+
+
+def transcation_isolation(func):
+    @wraps(func)
+    def func_wrapper(self, *args, **kwargs):
+        with Session(self.engine) as session:
+            return func(self, *args, **kwargs, session=session)
+
 class Engine:
     def _get_config(self, config_file_name: str) -> dict:
-			config = ConfigParser.ConfigParser().read(config_file_name)
-			return config.__dict__
+        config = ConfigParser.ConfigParser().read(config_file_name)
+        return config.__dict__
 
-    def __init__(self, config_file_name: str = "config.ini", config_prefix: str = "bridge") -> None:
-        self.config = self._get_config()
-
-        self.engine = engine_from_config(self.config, prefix=config_prefix)
-    
+    def __init__(self, config_file_name: str = "config.ini", config_prefix: str = "db") -> None:
+        self.config = self._get_config(config_file_name=config_file_name)
+        self.engine = engine_from_config(
+            self.config,
+            prefix=config_prefix,
+        )
