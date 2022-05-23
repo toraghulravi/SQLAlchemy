f13129c83e71aacd9c87c037e2cea6076280cfef
Raghu Ravi <raghul.roshan@gmail.com>
2022-05-23 12:21:32 +0530

    added fulfilment table settings

--- databases/fulfilment/fulfilment.py
+++ databases/fulfilment/fulfilment.py
@@ -1,2 +1,43 @@
-class Fulfilment:
-	pass
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.declarative import declarative_base
+from typing import Any
+
+from databases.common import transcation_isolation
+from databases.runner import Engine
+
+BASE = declarative_base()
+
+
+class Fulfilment(BASE):
+	__tablename__ = 'shipment'
+
+	ShipmentId = Column(Integer, primary_key=True)
+	CompanyId = Column(Integer)
+	WarehouseId = Column(Integer)
+	ShipmentStatusId = Column(Integer)
+
+class FulfilmentUtils(Engine):
+	def __init__(self, *args: Any, **kwargs: Any) -> None:
+		super().__init__(*args, **kwargs)
+
+	def __repr__(self) -> str:
+		return f"<ShipmentId(id='{self.ShipmentId}', CompanyId='{self.CompanyId}', WarehouseId={self.WarehouseId}, ShipmentStatusId={self.ShipmentStatusId}>"
+
+	@transcation_isolation
+	def create_shipment_table(self, session: Session) -> None:
+		BASE.metadata.create_all(self.engine)
+
+	@transcation_isolation
+	def insert_shipment(self, session: Session) -> None:
+		shipment = Fulfilment(
+			ShipmentId=1,
+			CompanyId=1,
+			WarehouseId=1,
+			ShipmentStatusId=1
+		)
+		session.add(shipment)
+
+	@transcation_isolation
+	def get_shipment_count(self, session: Session) -> str:
+		return session.query(Fulfilment).count()
